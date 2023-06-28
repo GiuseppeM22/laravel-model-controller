bee59a5ca4eba55d916cea3581a60676b79aeefc
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-06-28 14:50:12 +0200

    add style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<h1>I Miei Film</h1>
 <div class="container my-3">
- <ul>
     @foreach ($movies as $movie)
-        <li>{{$movie->title}}</li>
+    <div class="cardsContainer">
+        <div class="carde">
+            <div class="infoContainer ">
+                <h5 class="name">{{$movie->title}}</h5>
+                <h6 class="type">{{$movie->original_title}}</h6>
+                <p class="type">{{$movie->nationality}}</p>
+                <div class="stars">
+                    <span>Rated: {{$movie->vote}}</span>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
     @endforeach
- </ul>
+
 
 </div>
 @endsection
